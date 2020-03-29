450b33402128436e9f36902a10b5b5bff66562ef
Carley Tripp <carley.tripp@outlook.com>
2020-03-29 02:01:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(obj, key, value){
-  let newDriver = Object.assign(driver, obj, key, value)
+  let newDriver = Object.assign({}, driver, {key: value})
 
   return newDriver
 }
